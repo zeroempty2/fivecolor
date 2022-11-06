799718d75787873773cddcd52d39e2e762b53d86
leeyoungbin <2zeroempty@gmail.com>
2022-11-06 20:19:55 +0900

    메인,프로필 기능 통일, 비밀번호 기능 추가

--- app.py
+++ app.py
@@ -23,19 +23,22 @@ def post_comment():
     comment_receive = request.form['comment_give']
     name_receive = request.form['name_give']
     time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
 
     doc = {
         'comment': comment_receive,
         'name': name_receive,
-        'time': time_receive
+        'time': time_receive,
+        'password': password_receive,
     }
-    comment_id = db.comments.insert_one(doc).inserted_id
 
-    return jsonify({'comment_id': str(comment_id)})
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료'})
 
 @app.route('/comments/delete', methods=['POST'])
 def delete_comment():
-    comment_id_receive = request.form['comment_id_give']
+    comment_id_receive = request.form['comment_give']
     db.comments.delete_one({'_id': ObjectId(comment_id_receive)})
 
     return jsonify({'msg': '댓글 삭제 완료'})
@@ -52,128 +55,241 @@ def update_comment():
 def popup1():
     return render_template('Profile_SW.html')
 
+
+
+@app.route("/comments_SW", methods=["POST"])
+def comment_post1():
+    comment_receive = request.form['comment_give']
+    name_receive = request.form['name_give']
+    time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
+
+    doc = {
+        'comment' : comment_receive,
+        'name' : name_receive,
+        'time' : time_receive,
+        'password' : password_receive,
+    }
+
+    db.comments_SW.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료'})
+
+@app.route('/update_commentSW', methods=['POST'])
+def update_commentSW():
+    comment_id_receive = ObjectId(request.form['comment_id_give'])
+    comment_receive = request.form['comment_give']
+
+    db.comments_SW.update_one({'_id': comment_id_receive}, {'$set': {'comment': comment_receive}})
+
+    return jsonify({'msg': '방명록 수정 완료'})
+
+
+@app.route("/delete_SW", methods=['POST'])
+def delete_comment1():
+    delete_receive = request.form['comment_give']
+    db.comments_SW.delete_one({'_id': ObjectId(delete_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+@app.route("/comments_SW", methods=["GET"])
+def comments_get1():
+    comments = list(db.comments_SW.find({}))
+    for comment in comments:
+        comment['_id'] = str(comment['_id'])
+
+    return jsonify({'comments': comments})
+
 @app.route('/popup2')
 def popup2():
     return render_template('Profile_SE.html')
+
+
+
+@app.route("/comments_SE", methods=["POST"])
+def comment_post2():
+    comment_receive = request.form['comment_give']
+    name_receive = request.form['name_give']
+    time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
+
+    doc = {
+        'comment' : comment_receive,
+        'name' : name_receive,
+        'time' : time_receive,
+        'password' : password_receive,
+    }
+
+    db.comments_SE.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료'})
+
+@app.route('/update_commentSE', methods=['POST'])
+def update_commentSE():
+    comment_id_receive = ObjectId(request.form['comment_id_give'])
+    comment_receive = request.form['comment_give']
+
+    db.comments_SE.update_one({'_id': comment_id_receive}, {'$set': {'comment': comment_receive}})
+
+    return jsonify({'msg': '방명록 수정 완료'})
+
+
+@app.route("/delete_SE", methods=['POST'])
+def delete_comment2():
+    delete_receive = request.form['comment_give']
+    db.comments_SE.delete_one({'_id': ObjectId(delete_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+@app.route("/comments_SE", methods=["GET"])
+def comments_get2():
+    comments = list(db.comments_SE.find({}))
+    for comment in comments:
+        comment['_id'] = str(comment['_id'])
+
+    return jsonify({'comments': comments})
 
 @app.route('/popup3')
 def popup3():
     return render_template('Profile_YB.html')
 
 
+
+@app.route("/comments_YB", methods=["POST"])
+def comment_post3():
+    comment_receive = request.form['comment_give']
+    name_receive = request.form['name_give']
+    time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
+
+    doc = {
+        'comment' : comment_receive,
+        'name' : name_receive,
+        'time' : time_receive,
+        'password' : password_receive,
+    }
+
+    db.comments_YB.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료'})
+
+@app.route('/update_commentYB', methods=['POST'])
+def update_commentsYB():
+    comment_id_receive = ObjectId(request.form['comment_id_give'])
+    comment_receive = request.form['comment_give']
+
+    db.comments_YB.update_one({'_id': comment_id_receive}, {'$set': {'comment': comment_receive}})
+
+    return jsonify({'msg': '방명록 수정 완료'})
+
+
+@app.route("/delete_YB", methods=['POST'])
+def delete_comment3():
+    delete_receive = request.form['comment_give']
+    db.comments_YB.delete_one({'_id': ObjectId(delete_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+@app.route("/comments_YB", methods=["GET"])
+def comments_get3():
+    comments = list(db.comments_YB.find({}))
+    for comment in comments:
+        comment['_id'] = str(comment['_id'])
+
+    return jsonify({'comments': comments})
+
 @app.route('/popup4')
 def popup4():
     return render_template('Profile_SJ.html')
+
+
+
+@app.route("/comments_SJ", methods=["POST"])
+def comment_post4():
+    comment_receive = request.form['comment_give']
+    name_receive = request.form['name_give']
+    time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
+
+    doc = {
+        'comment' : comment_receive,
+        'name' : name_receive,
+        'time' : time_receive,
+        'password' : password_receive,
+    }
+
+    db.comments_SJ.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료'})
+
+@app.route('/update_commentSJ', methods=['POST'])
+def update_commentsSJ():
+    comment_id_receive = ObjectId(request.form['comment_id_give'])
+    comment_receive = request.form['comment_give']
+
+    db.comments_SJ.update_one({'_id': comment_id_receive}, {'$set': {'comment': comment_receive}})
+
+    return jsonify({'msg': '방명록 수정 완료'})
+
+
+@app.route("/delete_SJ", methods=['POST'])
+def delete_comment4():
+    delete_receive = request.form['comment_give']
+    db.comments_SJ.delete_one({'_id': ObjectId(delete_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+@app.route("/comments_SJ", methods=["GET"])
+def comments_get4():
+    comments = list(db.comments_SJ.find({}))
+    for comment in comments:
+        comment['_id'] = str(comment['_id'])
+
+    return jsonify({'comments': comments})
 
 @app.route('/popup5')
 def popup5():
     return render_template('Profile_YJ.html')
 
-@app.route("/comments_SW", methods=["POST"])
-def comment_post1():
-    comment_receive = request.form['comment_give']
-    doc = {
-        'comment' : comment_receive,
-    }
-    db.comments_SW.insert_one(doc)
 
-    return jsonify({'msg': '작성 완료!'})
-
-@app.route("/comments_SE", methods=["POST"])
-def comments_post2():
-    comment_receive = request.form['comment_give']
-    doc = {
-        'comment': comment_receive,
-    }
-    db.comments_SE.insert_one(doc)
-
-    return jsonify({'msg': '작성 완료!'})
-
-@app.route("/comments_YB", methods=["POST"])
-def comments_post3():
-    comment_receive = request.form['comment_give']
-    doc = {
-        'comment': comment_receive,
-    }
-    db.comments_YB.insert_one(doc)
-
-    return jsonify({'msg': '작성 완료!'})
-
-@app.route("/comments_SJ", methods=["POST"])
-def comments_post4():
-    comment_receive = request.form['comment_give']
-    doc = {
-        'comment': comment_receive,
-    }
-    db.comments_SJ.insert_one(doc)
-
-    return jsonify({'msg': '작성 완료!'})
 
 @app.route("/comments_YJ", methods=["POST"])
-def comments_post5():
+def comment_post5():
     comment_receive = request.form['comment_give']
+    name_receive = request.form['name_give']
+    time_receive = request.form['time_give']
+    password_receive = request.form['password_give']
+
     doc = {
-        'comment': comment_receive,
+        'comment' : comment_receive,
+        'name' : name_receive,
+        'time' : time_receive,
+        'password' : password_receive,
     }
+
     db.comments_YJ.insert_one(doc)
 
-    return jsonify({'msg': '작성 완료!'})
+    return jsonify({'msg': '작성 완료'})
 
-@app.route("/delete_SW", methods=['POST'])
-def delete_comment1():
-    delete_receive = request.form['comment_give']
-    db.comments_SW.delete_one({'comment': delete_receive})
-    return jsonify({'msg': '삭제 완료!'})
+@app.route('/update_commentYJ', methods=['POST'])
+def update_commentsYJ():
+    comment_id_receive = ObjectId(request.form['comment_id_give'])
+    comment_receive = request.form['comment_give']
 
-@app.route("/delete_SE", methods=['POST'])
-def delete_comment2():
-    delete_receive = request.form['comment_give']
-    db.comments_SE.delete_one({'comment': delete_receive})
-    return jsonify({'msg': '삭제 완료!'})
+    db.comments_YJ.update_one({'_id': comment_id_receive}, {'$set': {'comment': comment_receive}})
 
-@app.route("/delete_YB", methods=['POST'])
-def delete_comment3():
-    delete_receive = request.form['comment_give']
-    db.comments_YB.delete_one({'comment': delete_receive})
-    return jsonify({'msg': '삭제 완료!'})
+    return jsonify({'msg': '방명록 수정 완료'})
 
-@app.route("/delete_SJ", methods=['POST'])
-def delete_comment4():
-    delete_receive = request.form['comment_give']
-    db.comments_SJ.delete_one({'comment': delete_receive})
-    return jsonify({'msg': '삭제 완료!'})
 
 @app.route("/delete_YJ", methods=['POST'])
 def delete_comment5():
     delete_receive = request.form['comment_give']
-    db.comments_YJ.delete_one({'comment': delete_receive})
+    db.comments_YJ.delete_one({'_id': ObjectId(delete_receive)})
     return jsonify({'msg': '삭제 완료!'})
-
-@app.route("/comments_SW", methods=["GET"])
-def comments_get1():
-    comments_list = list(db.comments_SW.find({}, {'_id': False}))
-    return jsonify({'comments': comments_list})
-
-@app.route("/comments_SE", methods=["GET"])
-def comments_get2():
-    comments_list = list(db.comments_SE.find({}, {'_id': False}))
-    return jsonify({'comments': comments_list})
-
-@app.route("/comments_YB", methods=["GET"])
-def comments_get3():
-    comments_list = list(db.comments_YB.find({}, {'_id': False}))
-    return jsonify({'comments': comments_list})
-
-@app.route("/comments_SJ", methods=["GET"])
-def comments_get4():
-    comments_list = list(db.comments_SJ.find({}, {'_id': False}))
-    return jsonify({'comments': comments_list})
 
 @app.route("/comments_YJ", methods=["GET"])
 def comments_get5():
-    comments_list = list(db.comments_YJ.find({}, {'_id': False}))
-    return jsonify({'comments': comments_list})
+    comments = list(db.comments_YJ.find({}))
+    for comment in comments:
+        comment['_id'] = str(comment['_id'])
 
+    return jsonify({'comments': comments})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5005, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
--- static/Profile_SJ.js
+++ static/Profile_SJ.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
-    show_comment();
-});
 
-function show_comment() {
+function open_box() {
+    $('#comment-box').show();
+}
+function close_box() {
+    $('#comment-box').hide();
+}
+
+
+function show_commentsSJ() {
     $.ajax({
         type: 'GET',
-        url: '/comments',
+        url: '/comments_SJ',
         data: {},
         success: function (response) {
-            let rows = response['comments'];
+            let rows = response['comments']
             let length = rows.length;
-            $('#total-comment').text('댓글 ' + length);
+            $('#total-comment').text('방명록 ' + length);
 
             for (let i = 0; i < rows.length; i++) {
                 let comment_id = rows[i]['_id'];
-                let comment = rows[i]['comment'];
-                let name = rows[i]['name'];
-                let time = rows[i]['time'];
+                let comment = rows[i]['comment']
+                let name = rows[i]['name']
+                let time = rows[i]['time']
                 let password = rows[i]['password']
+
 
                 let temp_html = `<blockquote class="blockquote mb-0">
                                     <p id="${comment_id}">${comment}</p>
@@ -35,36 +41,22 @@ function show_comment() {
                                         <button onclick="update_comment('${comment_id}','${password}')" type="button" id="${comment_id}-update-btn" class="btn2 btn-outline-warning">수정</button>
                                         <button onclick="delete_comment('${comment_id}')" type="button" id="${comment_id}-delete-btn" class="btn4 btn-outline-danger">삭제</button>
                                         <button onclick="delete_comment_hidden('${comment_id}')" type="button" id="${comment_id}-delete" class="btn6 btn-outline-danger">삭제</button>
+                                        
                                     </form>
                                 </blockquote>
                                 <hr>`;
-                $('#comments-box').prepend(temp_html);
-               document.getElementById(comment_id + '-hidden').style.display = 'none';//댓글 수정창 숨기기
+                $('#comment_box').prepend(temp_html)
+                 document.getElementById(comment_id + '-hidden').style.display = 'none';//댓글 수정창 숨기기
                 document.getElementById(comment_id + '-password').style.display = 'none';
                 document.getElementById(comment_id + '-register-btn').style.display = 'none';//댓글 수정확인버튼 숨기기
                 document.getElementById(comment_id + '-original').style.display = 'none';
                 document.getElementById(comment_id + '-delete').style.display = 'none';
+
+
             }
-        },
+        }
     });
 }
-
-function post_comment() {
-    let comment = $('#comment').val()
-    let name = $('#name').val()
-    let time = new Date().toLocaleString('ko-KR')
-    let password = $('#pwd').val()
-    $.ajax({
-        type: 'POST',
-        url: '/comments',
-        data: {comment_give: comment, name_give: name, time_give: time, password_give: password},
-        success: function (response) {
-            alert(response['msg'])
-            window.location.reload();
-        }
-        });
-}
-
 function update_comment(comment_id) {
     document.getElementById(comment_id).style.display = 'none';
     document.getElementById(comment_id + '-hidden').style.display = 'block';
@@ -86,6 +78,9 @@ function delete_comment(comment_id) {
     document.getElementById(comment_id + '-delete').style.display = 'block';
 }
 
+
+
+
 function register_updated_comment(comment_id_hidden) {
     let comment = document.getElementById(comment_id_hidden).value;
     let pwd1 = $('#password').val();
@@ -96,7 +91,7 @@ function register_updated_comment(comment_id_hidden) {
             if (pwd1 == pwd2) {
                 $.ajax({
                     type: 'POST',
-                    url: '/comments/update',
+                    url: '/update_commentSJ',
                     data: {comment_id_give: comment_id_hidden.slice(0, -7), comment_give: comment},
                     success: function (response) {
                         alert(response['msg'])
@@ -118,6 +113,22 @@ function register_updated_comment(comment_id_hidden) {
     document.getElementById(password + '-password').style.display = 'none';
 }
 
+function save_comment() {
+    let comment = $('#comment').val()
+    let name = $('#name').val()
+    let time = new Date().toLocaleString('ko-KR')
+    let password = $('#pwd').val()
+    $.ajax({
+        type: 'POST',
+        url: '/comments_SJ',
+        data: {comment_give: comment, name_give: name, time_give: time, password_give: password},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload();
+        }
+        });
+}
+
 function delete_comment_hidden(comment_id) {
     let pwd1 = $('#password').val();
     let pwd2 = $('#password_update').val();
@@ -127,7 +138,7 @@ function delete_comment_hidden(comment_id) {
         if (pwd1 == pwd2) {
             $.ajax({
                 type: 'POST',
-                url: '/comments/delete',
+                url: '/delete_SJ',
                 data: {comment_give: comment_id},
                 success: function (response) {
                     alert(response['msg'])
@@ -142,31 +153,3 @@ function delete_comment_hidden(comment_id) {
 
 }
 
-
-function open_box() {
-    $('#comment-box').show();
-}
-
-function close_box() {
-    $('#comment-box').hide();
-}
-
-function show_member1(){
-    window.open("/popup1" ,"_blank", "width=2600,height=2100");
-}
-
-function show_member2(){
-    window.open("/popup2" , "_blank", "width=2600,height=2100");
-}
-
-function show_member3(){
-    window.open("/popup3" , "_blank", "width=2600,height=2100");
-}
-
-function show_member4(){
-    window.open("/popup4" , "_blank", "width=2600,height=2100");
-}
-
-function show_member5(){
-    window.open("/popup5" , "_blank", "width=2600,height=2100");
-}
